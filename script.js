133613062c23db34761371f77ce9a9bd792cd55c
Glitch (maquetandocongrid2018) <none>
2018-07-04 20:42:39 +0000

    🚩☕️ Updated with Glitch

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 class Nota{
   
   static agregar(notita){
-    this._db.push();
+    let lanota = {};
+    lanota.titulo = document.querySelector('#el_titulo').value;
+    lanota.contenido = document.querySelector('#el_contenido').value;
+    
+    let inicializoNotas =  localStorage.getItem("notas");
+    if ( inicializoNotas === null ){
+      inicializoNotas = "[]";
+    }
+    let listaNotas = JSON.parse( inicializoNotas ); 
+    listaNotas.push( lanota );
+    localStorage.setItem( "notas", JSON.stringify( listaNotas ) );    
   }
   
 }
